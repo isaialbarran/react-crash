b09b40076fe268c17c2f7295cf6a2f96c609fdc5
Isai Albarran <isaialbarran@gmail.com>
2020-09-21 16:23:23 +0200

    add docs

---
+++ docs/precache-manifest.285580fd6028bab37e09ab6df279a5cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71caca474ec3588ab55d",
+    "url": "/static/css/main.7b1a5910.chunk.css"
+  },
+  {
+    "revision": "71caca474ec3588ab55d",
+    "url": "/static/js/main.71caca47.chunk.js"
+  },
+  {
+    "revision": "1e90a09d958999e4a955",
+    "url": "/static/js/1.1e90a09d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97477dbaa76cd89b748864b3a15dd8a5",
+    "url": "/index.html"
+  }
+];
